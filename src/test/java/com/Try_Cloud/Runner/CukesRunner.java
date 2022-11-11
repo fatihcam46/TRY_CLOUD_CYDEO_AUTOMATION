7f9e8d63084f9f0a26d1d6e650b9227c3ed8caac
Ensar_Zorlu <zorlu.qa.test@gmail.com>
2022-11-11 22:02:18 +0000

    Added AC3-4-5

--- src/test/java/com/Try_Cloud/Runner/CukesRunner.java
+++ src/test/java/com/Try_Cloud/Runner/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         glue = "com/Try_Cloud/Step_Definition",
         features = "src/test/java/Resources",
         dryRun = false,
-        tags = "@LogIN"
+        tags = "@CLOUD-1205"
 
 )
 public class CukesRunner {
